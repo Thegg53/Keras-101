67db0cc22b61e461f202cd9667b88ecfa48b5413
Guilherme Morgado <grmorgado@gmail.com>
2016-04-14 13:35:04 +0100

    Added files via upload
    CNN carros e barcos
    As pastas com training data e test data chamam-se: cnn_data_train  cnn_data_test
    Cada pasta tem de ter Jpegs e um ficheiro labels txt com o formato:
    imgname.jpeg label#
    
    label# é um inteiro
    existe na pasta um ficheiro txt chamado keys que relaciona os numeros das labels com palavras

---
+++ img_loader_train.py
@@ -0,0 +1,52 @@
+import os, os.path
+import tensorflow as tf
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+def train():
+    #name of folder where train images are located
+    D='cnn_data_train'
+    #list files in folder
+    file_list=sorted(os.listdir(D))
+    #extract txt file => labels
+    txt_list=[]
+    for file in file_list:
+        if file.endswith("labels.txt"):
+            txt_list.append(file)
+            #print txt_list
+    #extract jpg files =>images
+    jpg_list=[]
+    for file in file_list:
+        if file.endswith(".jpg"):
+            jpg_list.append(file)
+            #print jpg_list
+
+    nb_train_samples=len(jpg_list)
+
+    #read labels file line by line
+    text_file = open(D+'/'+txt_list[0], "r")
+    lines = text_file.readlines()
+    text_file.close()
+    #split_lines=[]
+    img_name=[]
+    labels_train=[]
+    for i in range(len (lines)):
+        w,z=lines[i].split()
+        img_name.append(w)
+        labels_train.append(z)   
+    Y_train=labels_train
+    nb_classes=len(list(set(Y_train)))
+
+    # reshape_dimension=32
+    rs=32
+    X_train = np.zeros((nb_train_samples, rs, rs,3), dtype="float32")
+
+    for i in range(nb_train_samples):
+        img = Image.open(D+'/'+jpg_list[i])    # Open image as PIL image object
+        rsize = img.resize((rs,rs)) # Use PIL to resize
+        rsizeArr = np.asarray(rsize)  # Get array back
+        X_train[i,:,:,:]=rsizeArr
+    X_train=np.ndarray.transpose(X_train,(0,3,2,1))
+    return X_train,Y_train,nb_classes
